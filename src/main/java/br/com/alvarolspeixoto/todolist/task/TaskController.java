625410e15c33604f47b4bb78946c41af0b2a5777
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2023-10-12 18:28:56 -0300

    implement date validation

--- src/main/java/br/com/alvarolspeixoto/todolist/task/TaskController.java
+++ src/main/java/br/com/alvarolspeixoto/todolist/task/TaskController.java
@@ -1,10 +1,17 @@
 package br.com.alvarolspeixoto.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,10 +21,25 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+
+        var userId = request.getAttribute("userId");
+        taskModel.setUserId((UUID) userId);
+
+        var currentDate = LocalDateTime.now();
+        var startsAt = taskModel.getStartsAt();
+        var endsAt = taskModel.getEndsAt();
+
+        if(currentDate.isAfter(startsAt) || currentDate.isAfter(endsAt)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body("As datas de início e de fim devem ser posteriores à data atual");
+        } else if (startsAt.isAfter(endsAt)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body("A data de término deve ser posterior à data de início");
+        }
 
         var task = this.taskRepository.save(taskModel);
-
-        return task;
+        
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
